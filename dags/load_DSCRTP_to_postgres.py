ed1b6d57b101db66cc7786f96cbc016a7152b494
Thomas Morrow <thomas.a.morrow.1@gmail.com>
2025-01-26 10:01:16 -0500

    update

--- dags/load_DSCRTP_to_postgres.py
+++ dags/load_DSCRTP_to_postgres.py
@@ -2,8 +2,6 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from datetime import datetime
-import pandas as pd
-import duckdb
 
 # Define default args for the DAG
 default_args = {
@@ -11,24 +9,13 @@ default_args = {
     'depends_on_past': False,
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1
+    'retries': 0
 }
 
-# Define the DAG
-dag = DAG(
-    'load_DSCRTP_to_postgres',
-    default_args=default_args,
-    description='Load DSCRTP CSV into PostgreSQL with schema creation',
-    schedule_interval=None,
-    start_date=datetime(2025, 1, 1),
-    catchup=False
-)
-
-# these need to be checked against the DSCRTP csv
 def create_table():
     create_table_query = """
     CREATE TABLE IF NOT EXISTS dscrtp_data (
-        "Unnamed: 0" INTEGER,
+        "Entry" INTEGER,
         "Flag" INTEGER,
         "FlagReason" FLOAT,
         "ShallowFlag" INTEGER,
@@ -50,20 +37,20 @@ def create_table():
         "LifeScienceIdentifier" TEXT,
         "Phylum" TEXT,
         "Class" TEXT,
-        "Subclass" FLOAT,
+        "Subclass" TEXT,
         "Order" TEXT,
         "Suborder" TEXT,
         "Family" TEXT,
-        "Subfamily" FLOAT,
+        "Subfamily" TEXT,
         "Genus" TEXT,
-        "Subgenus" FLOAT,
+        "Subgenus" TEXT,
         "Species" TEXT,
-        "Subspecies" FLOAT,
+        "Subspecies" TEXT,
         "ScientificNameAuthorship" TEXT,
         "TypeStatus" TEXT,
-        "OperationalTaxonomicUnit" INTEGER,
-        "Morphospecies" FLOAT,
-        "CombinedNameID" FLOAT,
+        "OperationalTaxonomicUnit" TEXT,
+        "Morphospecies" TEXT,
+        "CombinedNameID" TEXT,
         "Synonyms" TEXT,
         "IdentificationComments" TEXT,
         "IdentifiedBy" TEXT,
@@ -88,58 +75,22 @@ def create_table():
         "ObservationTime" FLOAT,
         "SurveyID" TEXT,
         "Vessel" TEXT,
-        "PI" FLOAT,
+        "PI" TEXT,
         "PIAffiliation" TEXT,
-        "Purpose" FLOAT,
-        "SurveyComments" FLOAT,
+        "Purpose" TEXT,
+        "SurveyComments" TEXT,
         "Station" INTEGER,
         "EventID" FLOAT,
-        "SamplingEquipment" FLOAT,
-        "VehicleName" FLOAT,
-        "SampleAreaInSquareMeters" INTEGER,
-        "footprintWKT" FLOAT,
-        "footprintSRS" FLOAT,
-        "IndividualCount" INTEGER,
-        "CategoricalAbundance" TEXT,
-        "Density" INTEGER,
-        "Cover" FLOAT,
-        "VerbatimSize" FLOAT,
-        "MinimumSize" INTEGER,
-        "MaximumSize" INTEGER,
-        "WeightInKg" INTEGER,
-        "Condition" TEXT,
-        "AssociatedTaxa" FLOAT,
-        "OccurrenceComments" FLOAT,
-        "StartLatitude" INTEGER,
-        "StartLongitude" INTEGER,
-        "EndLatitude" INTEGER,
-        "EndLongitude" INTEGER,
-        "VerbatimLatitude" FLOAT,
-        "VerbatimLongitude" FLOAT,
-        "LocationAccuracy" FLOAT,
-        "NavType" FLOAT,
-        "OtherData" FLOAT,
-        "Habitat" FLOAT,
-        "Substrate" FLOAT,
-        "CMECSGeoForm" FLOAT,
-        "CMECSSubstrate" FLOAT,
-        "CMECSBiotic" FLOAT,
-        "Temperature" INTEGER,
-        "Salinity" INTEGER,
-        "Oxygen" INTEGER,
-        "pH" INTEGER,
-        "pHscale" FLOAT,
-        "pCO2" INTEGER,
-        "TA" INTEGER,
-        "DIC" INTEGER,
+        "TA" FLOAT,
+        "DIC" FLOAT,
         "RecordType" TEXT,
-        "ImageFilePath" FLOAT,
-        "HighlightImageFilePath" FLOAT,
+        "ImageFilePath" TEXT,
+        "HighlightImageFilePath" TEXT,
         "DataProvider" TEXT,
         "DataContact" TEXT,
         "Modified" TEXT,
         "EntryUpdate" TEXT,
-        "WebSite" FLOAT,
+        "WebSite" TEXT,
         "EntryDate" TEXT,
         "Reporter" TEXT,
         "ReporterEmail" TEXT,
@@ -166,35 +117,39 @@ def create_table():
         "gisWesternPacificFMC" INTEGER
     );
     """
-
-    # Execute the query using PostgresHook
     postgres_hook = PostgresHook(postgres_conn_id='postgres_default')
     postgres_hook.run(create_table_query)
 
 def load_csv_to_postgres():
-    # Load CSV and clean data
-    csv_path = '/mnt/data/DSCRTP_NatDB_20241022-1_toprows.csv'
-    duckdb_query = """
-    SELECT *
-    FROM read_csv_auto(?, header=True, na_strings=['NA', '-999'])
+    # Specify the CSV file path (it must be accessible to the PostgreSQL server)
+    csv_path = '/tmp/DSCRTP_NatDB.csv'
+
+    # COPY command to load CSV data into PostgreSQL
+    copy_query = f"""
+    COPY dscrtp_data FROM '{csv_path}' WITH CSV HEADER DELIMITER ',' NULL 'NA';
     """
-    df = duckdb.query(duckdb_query, [csv_path]).to_df()
-
-    # Insert data into PostgreSQL
+    
+    # PostgreSQL hook to run the COPY query
     postgres_hook = PostgresHook(postgres_conn_id='postgres_default')
-    postgres_hook.insert_rows('dscrtp_data', df.values.tolist(), target_fields=df.columns.tolist())
+    postgres_hook.run(copy_query)
 
-# Define tasks
-create_table_task = PythonOperator(
-    task_id='create_table',
-    python_callable=create_table,
-    dag=dag
-)
+with DAG(
+    dag_id='dataset_load_DSCRTP_to_postgres',
+    default_args=default_args,
+    description='Load DSCRTP CSV into PostgreSQL with schema creation',
+    schedule_interval=None,
+    start_date=datetime(2025, 1, 1),
+    catchup=False
+) as dag:
 
-load_csv_task = PythonOperator(
-    task_id='load_csv_to_postgres',
-    python_callable=load_csv_to_postgres,
-    dag=dag
-)
+    create_table_task = PythonOperator(
+        task_id='create_table',
+        python_callable=create_table
+    )
 
-create_table_task >> load_csv_task
+    load_csv_task = PythonOperator(
+        task_id='load_csv_to_postgres',
+        python_callable=load_csv_to_postgres
+    )
+
+    create_table_task >> load_csv_task
