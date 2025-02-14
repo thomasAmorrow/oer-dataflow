89d5786026785c5e2711c4cd593b2c137b4f4646
Thomas Morrow <thomas.a.morrow.1@gmail.com>
2025-02-14 13:35:41 +0000

    Docker compose edited to properly use passwords in .env for airflow's postgresql database.

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # init script for airflow setup
-mkdir -p ./dags ./logs ./plugins ./config # only needed if you didn't clone the dirs
+#mkdir -p ./dags ./logs ./plugins ./config # only needed if you didn't clone the dirs
 echo -e "AIRFLOW_UID=$(id -u)" > .env
 
-docker compose up airflow-init # initialize database, should exit with code 0
+#docker compose up airflow-init # initialize database, should exit with code 0
 
 #docker compose down --volumes --remove-orphans # cleanup after running if you have big problems
