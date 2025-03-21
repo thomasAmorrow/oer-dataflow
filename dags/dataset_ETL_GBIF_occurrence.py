7648daa4b52a9792d6428ae505bcf2b078934804
Thomas Morrow <thomas.a.morrow.1@gmail.com>
2025-03-21 14:13:10 -0400

    First pass at GBIF run, limit 5 hexagons.

--- dags/dataset_ETL_GBIF_occurrence.py
+++ dags/dataset_ETL_GBIF_occurrence.py
@@ -1,74 +1,171 @@
+import logging
+import geopandas as gpd
+import pandas as pd
+import h3
+from shapely.geometry import shape
+from shapely.wkt import loads, dumps
+from pygbif import occurrences as occ
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import datetime, timedelta
-import os
-import csv
-import pygbif
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from sqlalchemy import create_engine
-from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
 
-# Default arguments for DAG
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 0,
-    'retry_delay': timedelta(minutes=5),
-}
 
-# Function to load H3 data to PostgreSQL
-def function1(argument, argument, anotherargument):
-    python code things
+# Function to check if a polygon's coordinates are counter-clockwise
+def is_counter_clockwise(polygon_wkt):
+    polygon = loads(polygon_wkt)
+    return polygon.exterior.is_ccw
+
+# Function to rearrange polygon to counter-clockwise
+def rearrange_to_counter_clockwise(polygon_wkt):
+    polygon = loads(polygon_wkt)
+    if not polygon.exterior.is_ccw:
+        polygon = Polygon(list(polygon.exterior.coords)[::-1])
+    return dumps(polygon)
+
+
+def fetch_and_save_occurrences(h3_index, postgres_conn_id='oceexp-db'):
+    # Connect to PostgreSQL
+    pg_hook = PostgresHook(postgres_conn_id)
+    conn = pg_hook.get_conn()
+    cursor = conn.cursor()
+
+    # Get polygon geometry
+    polygon = h3.cells_to_geo([h3_index], tight=True)
+    polygeo = shape(polygon)
+
+    # If the polygon is not counter-clockwise, rearrange it
+    if not polygeo.exterior.is_ccw:
+        logging.info(f"Rearranging polygon for H3 index {h3_index} to counter-clockwise")
+        polygon_wkt = dumps(polygeo)
+        rearranged_wkt = rearrange_to_counter_clockwise(polygon_wkt)
+        polygeo = shape(loads(rearranged_wkt))
+
+    # Search for occurrences in the polygon
+    critters = occ.search(geometry=polygeo.wkt, limit=10000, depth='200,12000', fields=[
+        'latitude', 'longitude', 'depth', 'taxonKey', 'scientificName', 'kingdomKey', 'phylumKey',
+        'classKey', 'orderKey', 'familyKey', 'genusKey', 'basisOfRecord'])
+
+    occurrences = []
+
+    # Extract data for each occurrence
+    for critter in critters['results']:
+        latitude = critter['decimalLatitude']
+        longitude = critter['decimalLongitude']
+        depth = critter['depth']
+        taxonkey = critter['taxonKey']
+        scientificname = critter['scientificName']
+        kingdom = critter.get('kingdomKey', None)
+        phylum = critter.get('phylumKey', None)
+        class_key = critter.get('classKey', None)
+        order = critter.get('orderKey', None)
+        family = critter.get('familyKey', None)
+        genus = critter.get('genusKey', None)
+        basisofrecord = critter.get('basisOfRecord', None)
+
+        if depth is not None:
+            occurrences.append({
+                'latitude': latitude,
+                'longitude': longitude,
+                'depth': depth,
+                'taxonkey': taxonkey,
+                'scientificname': scientificname,
+                'kingdomKey': kingdom,
+                'phylumKey': phylum,
+                'classKey': class_key,
+                'orderKey': order,
+                'familyKey': family,
+                'genusKey': genus,
+                'basisofrecord': basisofrecord,
+                'h3_index': h3_index  # Add the h3 index to the occurrences
+            })
+
+    # Convert occurrences to DataFrame
+    occurrences_df = pd.DataFrame(occurrences)
+    if occurrences_df.empty:
+        logging.warning(f"No occurrences found for H3 index {h3_index}")
+        return
+
+    # Insert occurrences into PostgreSQL
+    for _, row in occurrences_df.iterrows():
+        cursor.execute("""
+            INSERT INTO hexagon_occurrences (latitude, longitude, depth, taxonkey, scientificname, kingdomKey,
+            phylumKey, classKey, orderKey, familyKey, genusKey, basisofrecord, hex_05)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """, (row['latitude'], row['longitude'], row['depth'], row['taxonkey'], row['scientificname'],
+                  row['kingdomKey'], row['phylumKey'], row['classKey'], row['orderKey'], row['familyKey'],
+                  row['genusKey'], row['basisofrecord'], row['h3_index']))
+    conn.commit()
+    logging.info(f"Inserted {len(occurrences_df)} occurrences for H3 index {h3_index} into database.")
+
+
+def fetch_h3_indices_and_create_table(postgres_conn_id='oceexp-db'):
+    # Connect to PostgreSQL and fetch the list of H3 indices
+    pg_hook = PostgresHook(postgres_conn_id)
+    conn = pg_hook.get_conn()
+    cursor = conn.cursor()
+
+    # Fetch all hexagon H3 indices
+    cursor.execute("SELECT DISTINCT hex_05 FROM h3_oceans LIMIT 20") # limit to 20 for testing/dev
+    indices = cursor.fetchall()
+
+    # Create the results table if it doesn't exist
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS gbif_occurrences (
+            id SERIAL PRIMARY KEY,
+            latitude DOUBLE PRECISION,
+            longitude DOUBLE PRECISION,
+            depth DOUBLE PRECISION,
+            taxonkey INT,
+            scientificname TEXT,
+            kingdomKey INT,
+            phylumKey INT,
+            classKey INT,
+            orderKey INT,
+            familyKey INT,
+            genusKey INT,
+            basisofrecord TEXT,
+            hex_05 H3INDEX
+        )
+    """)
+    conn.commit()
+
+    # Return the list of H3 indices
+    return [index[0] for index in indices]
+
 
 # Define the DAG
 dag = DAG(
-    'its_my_dag',
-    default_args=default_args,
-    description='DAG to do the DAG',
-    schedule_interval=None,  # Trigger manually or modify as needed
-    start_date=datetime(2025, 3, 13),
-    catchup=False,
+    'dataset_ETL_GBIF_occurrence',
+    description='Fetch occurrences for H3 hexagons and save to PostgreSQL',
+    schedule_interval='@daily',
+    start_date=datetime(2025, 3, 21),
+    catchup=False
 )
 
-# Define task arguments
-URL_I_NEED = "http://url.i.need"
-CSV_I_NEED = "csvineed.csv"
-
-# task 1 python operator
-task1 = PythonOperator(
-    task_id='task1',
-    python_callable=function1,
-    op_kwargs={
-        'csv_path': OUTPUT_CSV,
-        'table_name': 'table',
-        'postgres_conn_id': 'connection',
-    },
-    dag=dag,
+# Task to fetch H3 indices
+fetch_indices_task = PythonOperator(
+    task_id='fetch_h3_indices',
+    python_callable=fetch_h3_indices_and_create_table,
+    provide_context=True,
+    dag=dag
 )
 
-# task 2 python operator
-task2 = PythonOperator(
-    task_id='task2',
-    python_callable=function2,
-    op_kwargs={
-        'csv_path': OUTPUT_CSV,
-        'table_name': 'table',
-        'postgres_conn_id': 'connection',
-    },
-    dag=dag,
+# Task to fetch occurrences for each H3 index
+def fetch_occurrences_for_each_hexagon(**kwargs):
+    # Get H3 indices from previous task
+    indices = kwargs['ti'].xcom_pull(task_ids='fetch_h3_indices')
+    
+    for h3_index in indices:
+        fetch_and_save_occurrences(h3_index)
+
+
+fetch_occurrences_task = PythonOperator(
+    task_id='fetch_occurrences_for_each_hexagon',
+    python_callable=fetch_occurrences_for_each_hexagon,
+    provide_context=True,
+    dag=dag
 )
 
-# Task3 postgresql operator
-task3 = PostgresOperator(
-    task_id='task3',
-    postgres_conn_id='connection',  # Define your connection ID
-    sql="""
-        DO SOME SQL HERE
-    """,
-)
-
-
-# Set task dependencies
-task1 >> task2 >> task3 
+# Define task dependencies
+fetch_indices_task >> fetch_occurrences_task
--- pygbif_query.py
+++ pygbif_query.py
@@ -5,6 +5,7 @@ from shapely.geometry import Polygon, shape
 from shapely.wkt import loads, dumps
 from pygbif import occurrences as occ
 import h3
+import pandas as pd
 
 # Function to check if a polygon's coordinates are counter-clockwise
 def is_counter_clockwise(polygon_wkt):
@@ -19,7 +20,7 @@ def rearrange_to_counter_clockwise(polygon_wkt):
     return dumps(polygon)
 
 # gimme a cell near Hawaii
-cells = ['825d1ffffffffff']
+cells = ['835d12fffffffff']
 
 # gimme the polys
 polygon = h3.cells_to_geo(cells, tight=True)
@@ -40,13 +41,49 @@ if not polygeo.exterior.is_ccw:
     polygeo = shape(loads(rearranged_wkt))
 
 # Search for critters (occurrences) within the polygon
-critters = occ.search(geometry=polygeo.wkt, limit=10)
+critters = occ.search(geometry=polygeo.wkt, limit=20000, depth="200,10000", fields=['latitude','longitude','depth','taxonKey','scientificName', 'kingdomKey', 'phylumKey', 'classKey', 'orderKey', 'familyKey', 'genusKey', 'basisOfRecord'])
 #print("Critters found:", critters)
 
-# Extract latitude and longitude from the first critter
-latitude = critters['results'][0]['decimalLatitude']
-longitude = critters['results'][0]['decimalLongitude']
-print(f"Critter Location: Latitude = {latitude}, Longitude = {longitude}")
+# Prepare a list to store occurrences
+occurrences = []
+
+# Extract latitude, longitude, depth, and taxonomy information from the critters
+for critter in critters['results']:
+    latitude = critter['decimalLatitude']
+    longitude = critter['decimalLongitude']
+    depth = critter['depth']  # 'None' if depth is not available
+    taxonkey = critter['taxonKey']
+    scientificname = critter['scientificName']
+    kingdom = critter.get('kingdomKey', None)
+    phylum = critter.get('phylumKey', None)
+    class_key = critter.get('classKey', None)
+    order = critter.get('orderKey', None)
+    family = critter.get('familyKey', None)
+    genus = critter.get('genusKey', None)
+    basisofrecord = critter.get('basisOfRecord', None)
+    #subgenus = critter.get('subgenusKey', None)
+    
+    # Only add the critter to occurrences if depth is available
+    if depth is not None:
+        occurrences.append({
+            'latitude': latitude,
+            'longitude': longitude,
+            'depth': depth,
+            'taxonkey' : taxonkey,
+            'scientificname' : scientificname,
+            'kingdomKey': kingdom,
+            'phylumKey': phylum,
+            'classKey': class_key,
+            'orderKey': order,
+            'familyKey': family,
+            'genusKey': genus,
+            'basisofrecord' : basisofrecord
+            #'subgenusKey': subgenus
+        })
+
+# Convert occurrences list to DataFrame and print only those with depth
+occurrences_df = pd.DataFrame(occurrences)
+print(occurrences_df)
 
 # Plot the polygon on a map using GeoPandas and Matplotlib
 gdf = gpd.GeoDataFrame([polygeo], columns=['geometry'])
@@ -58,23 +95,19 @@ fig, ax = plt.subplots(figsize=(8, 8))
 # Plot the polygon
 gdf.plot(ax=ax, color='lightblue', edgecolor='black', alpha=0.7)
 
-# Add a basemap using contextily
 # Reproject to web mercator (EPSG:3857) for compatibility with contextily basemaps
-gdf = gdf.to_crs(epsg=4326)
+gdf = gdf.to_crs(epsg=3857)
 
 # Add OpenStreetMap basemap
 ctx.add_basemap(ax, crs=gdf.crs)
 
-# Plot the critter location as a dot
-ax.scatter(longitude, latitude, color='red', s=100, label='Critter Location')
+# Plot the critter locations as red dots
+for critter in occurrences:
+    ax.scatter(critter['longitude'], critter['latitude'], color='red', s=100, label='Critter Location')
 
 # Set plot title
 ax.set_title("Polygon Location Map with Critter", fontsize=15)
 
-# Optionally, adjust the extent of the plot to focus on the polygon
-#ax.set_xlim([-160,-150])  # xmin, xmax
-#ax.set_ylim([15, 25])  # ymin, ymax
-
 # Show the plot
-plt.legend()
+#plt.legend()
 plt.show()
