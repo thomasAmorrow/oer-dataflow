33405b410fe134c8040abd4e447f70c375c39c42
Thomas Morrow <thomas.a.morrow.1@gmail.com>
2025-03-25 13:28:05 -0400

    Syntax

--- dags/dataset_ETL_GBIF_occurrence.py
+++ dags/dataset_ETL_GBIF_occurrence.py
@@ -12,6 +12,7 @@ from airflow import DAG
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
+import os
 
 
 
