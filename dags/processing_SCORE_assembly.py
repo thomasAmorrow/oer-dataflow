36286ab6748d30137fc503b2cc4ccda0b0a0a4b5
Thomas Morrow <thomas.a.morrow.1@gmail.com>
2025-04-04 09:35:34 -0400

    Score setup for detailed mapping TID analysis (rolling back for testing)

--- dags/processing_SCORE_assembly.py
+++ dags/processing_SCORE_assembly.py
@@ -5,6 +5,17 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 from sqlalchemy import create_engine
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
+
+# Default arguments for DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5),
+}
+
 # Define the DAG
 dag = DAG(
     'processing_SCORE_assembly',
@@ -20,13 +31,42 @@ create_SCORE_table= PostgresOperator(
     task_id='create_SCORE_table',
     postgres_conn_id='oceexp-db',  # Define your connection ID
     sql="""
-    DROP TABLE IF EXISTS ega_score
+    DROP TABLE IF EXISTS ega_score_05
 
-    CREATE TABLE ega_score_05(
-    hex_05 H3INDEX PRIMARY KEY,
-    mapping_score FLOAT,
-    occurrence_score FLOAT,
-    chemistry_score FLOAT
+    CREATE TABLE ega_score_05 AS
+    SELECT hex_05
+    FROM h3_oceans;
+
+    ALTER TABLE ega_score_05
+    ADD COLUMN mapping_score FLOAT;
+
+    UPDATE ega_score_05
+    SET mapping_score = CASE
+                        WHEN m.val BETWEEN 10 AND 17 THEN 1
+                        WHEN m.val > 17 THEN 0.1
+                        ELSE 0
+                    END
+    FROM ega_score_05 s
+    LEFT JOIN gebco_tid_hex m
+    ON s.hex_05 = m.hex_05;
+
+
+    ALTER TABLE ega_score_05
+    ADD COLUMN occurrence_score FLOAT;
+
+    UPDATE ega_score_05
+    SET occurrence_score = 1
+    FROM gbif_occurrences
+    WHERE ega_score_05.hex_05 = gbif_occurrences.hex_05;
+
+    ALTER TABLE ega_score_05
+    ADD COLUMN chemistry_score FLOAT;
+
+    UPDATE ega_score_05
+    SET chemistry_score = 1
+    FROM glodap
+    WHERE ega_score_05.hex_05 = glodap.hex_05 
+
     )
     """,
 )
@@ -45,4 +85,4 @@ assemble_scores= PostgresOperator(
 
 
 # Define task dependencies
-create_SCORE_table >> assemble_scores
+create_SCORE_table
