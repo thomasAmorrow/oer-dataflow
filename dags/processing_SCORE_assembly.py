fdfeb784dfac6651a714452dfc31c15250018368
Thomas Morrow <thomas.a.morrow.1@gmail.com>
2025-04-16 13:13:06 -0400

    Revert, need a separate DAG for building parent cell scores

--- dags/processing_SCORE_assembly.py
+++ dags/processing_SCORE_assembly.py
@@ -1,8 +1,10 @@
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from sqlalchemy import create_engine
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
 
 # Default arguments for DAG
 default_args = {
@@ -24,79 +26,75 @@ dag = DAG(
     catchup=False,
 )
 
-# Function to create and combine scores for each hex value
-def process_scores_for_hex(**kwargs):
-    hex_values = ['hex_05', 'hex_04', 'hex_03']
-    postgres_hook = PostgresHook(postgres_conn_id='oceexp-db')
-    
-    for hex_value in hex_values:
-        # Create the scores table for the given hex value
-        create_sql = f"""
-        DROP TABLE IF EXISTS ega_score_{hex_value};
+# Task: Create the scores table
+create_primary_SCORE_table= PostgresOperator(
+    task_id='create_SCORE_table',
+    postgres_conn_id='oceexp-db',  # Define your connection ID
+    sql="""
+    DROP TABLE IF EXISTS ega_score_05;
 
-        CREATE TABLE IF NOT EXISTS ega_score_{hex_value} AS
-        SELECT {hex_value}
-        FROM h3_oceans;
+    CREATE TABLE IF NOT EXISTS ega_score_05 AS
+    SELECT hex_05
+    FROM h3_oceans;
 
-        ALTER TABLE ega_score_{hex_value}
-        ADD PRIMARY KEY ({hex_value});
+    ALTER TABLE ega_score_05
+    ADD PRIMARY KEY (hex_05);
 
-        ALTER TABLE ega_score_{hex_value}
-        ADD COLUMN mapping_score FLOAT;
+    ALTER TABLE ega_score_05
+    ADD COLUMN mapping_score FLOAT;
 
-        UPDATE ega_score_{hex_value}
-        SET mapping_score = 1
-        FROM gebco_tid_hex
-        WHERE ega_score_{hex_value}.{hex_value} = gebco_tid_hex.{hex_value} AND gebco_tid_hex.val BETWEEN 9 AND 18;
+    UPDATE ega_score_05
+    SET mapping_score = 1
+    FROM gebco_tid_hex
+    WHERE ega_score_05.hex_05 = gebco_tid_hex.hex_05 AND gebco_tid_hex.val BETWEEN 9 AND 18;
 
-        UPDATE ega_score_{hex_value}
-        SET mapping_score = 0.1
-        FROM gebco_tid_hex
-        WHERE ega_score_{hex_value}.{hex_value} = gebco_tid_hex.{hex_value} AND gebco_tid_hex.val BETWEEN 39 AND 47;
+    UPDATE ega_score_05
+    SET mapping_score = 0.1
+    FROM gebco_tid_hex
+    WHERE ega_score_05.hex_05 = gebco_tid_hex.hex_05 AND gebco_tid_hex.val BETWEEN 39 AND 47;
 
-        ALTER TABLE ega_score_{hex_value}
-        ADD COLUMN occurrence_score FLOAT;
+    ALTER TABLE ega_score_05
+    ADD COLUMN occurrence_score FLOAT;
 
-        UPDATE ega_score_{hex_value}
-        SET occurrence_score = 1
-        FROM gbif_occurrences
-        WHERE ega_score_{hex_value}.{hex_value} = gbif_occurrences.{hex_value};
+    UPDATE ega_score_05
+    SET occurrence_score = 1
+    FROM gbif_occurrences
+    WHERE ega_score_05.hex_05 = gbif_occurrences.hex_05;
 
-        ALTER TABLE ega_score_{hex_value}
-        ADD COLUMN chemistry_score FLOAT;
+    ALTER TABLE ega_score_05
+    ADD COLUMN chemistry_score FLOAT;
 
-        UPDATE ega_score_{hex_value}
-        SET chemistry_score = 1
-        FROM glodap
-        WHERE ega_score_{hex_value}.{hex_value} = glodap.{hex_value};
+    UPDATE ega_score_05
+    SET chemistry_score = 1
+    FROM glodap
+    WHERE ega_score_05.hex_05 = glodap.hex_05;
 
-        ALTER TABLE ega_score_{hex_value}
-        ADD COLUMN geology_score FLOAT;
+    ALTER TABLE ega_score_05
+    ADD COLUMN geology_score FLOAT;
 
-        UPDATE ega_score_{hex_value}
-        SET geology_score = 1
-        FROM imlgs
-        WHERE ega_score_{hex_value}.{hex_value} = imlgs.{hex_value};
-        """
-        
-        # Execute the creation SQL for the current hex value
-        postgres_hook.run(create_sql)
+    UPDATE ega_score_05
+    SET geology_score = 1
+    FROM imlgs
+    WHERE ega_score_05.hex_05 = imlgs.hex_05;
 
-        # Combine the scores for the given hex value
-        combine_sql = f"""
-            ALTER TABLE ega_score_{hex_value}
-            ADD COLUMN combined_score FLOAT;
-
-            UPDATE ega_score_{hex_value}
-            SET combined_score = (COALESCE(mapping_score, 0) + COALESCE(occurrence_score, 0) + COALESCE(chemistry_score, 0) + COALESCE(geology_score, 0)) / 4;
-        """
-        
-        # Execute the combine SQL for the current hex value
-        postgres_hook.run(combine_sql)
-
-# Define task to process all hex values
-process_scores = PythonOperator(
-    task_id='process_scores',
-    python_callable=process_scores_for_hex,
+    """,
     dag=dag,
 )
+
+# Task: Create the h3_children table
+combine_primary_scores= PostgresOperator(
+    task_id='combine_scores',
+    postgres_conn_id='oceexp-db',  # Define your connection ID
+    sql="""
+        ALTER TABLE ega_score_05
+        ADD COLUMN combined_score FLOAT;
+
+        UPDATE ega_score_05
+        SET combined_score = (COALESCE(mapping_score, 0) + COALESCE(occurrence_score, 0) + COALESCE(chemistry_score, 0) + COALESCE(geology_score, 0)) / 4;
+
+    """,
+    dag=dag,
+)
+
+# Define task dependencies
+create_primary_SCORE_table >> combine_primary_scores
