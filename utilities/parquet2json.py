35e6bccb6bfb19a343d76e014fef4362a3dac9f0
Thomas Morrow <thomas.a.morrow.1@gmail.com>
2025-05-08 10:07:03 -0400

    Compression and parquet files added with README notes for levels 3 and 4

---
+++ utilities/parquet2json.py
@@ -0,0 +1,13 @@
+import geopandas as gpd
+
+for i in ["03", "04", "05"]:
+    # Load GeoParquet
+    gdf = gpd.read_parquet(f"h3_hexagons_{i}.parquet")
+
+    # Save as GeoJSON
+    gdf.to_file(f"h3_hexagons_{i}.geojson", driver="GeoJSON")
+
+    gdf = gpd.read_parquet(f"h3_points_{i}.parquet")
+
+    # Save as GeoJSON
+    gdf.to_file(f"h3_points_{i}.geojson", driver="GeoJSON")
